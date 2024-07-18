61f2c7ddd20756dbb89609cd824063cffc229987
lurnie <lurnsp22@gmail.com>
2024-07-18 19:52:36 +0200

    Added more to user pages

--- frontend/public/scripts/user.js
+++ frontend/public/scripts/user.js
@@ -1,6 +1,14 @@
 import { getDateString } from "./getDateString.js";
 
+const container = document.querySelector('.user-container');
 const username = document.querySelector('.user-username');
 username.textContent = `@${params.user.username}`;
 const userJoined = document.querySelector('.user-joined');
 userJoined.textContent = `Joined ${getDateString(params.user.date_created)}`;
+
+if (params.user.admin_privileges) {
+    const admin = document.createElement('div');
+    admin.textContent = 'Admin';
+    admin.setAttribute('class', 'user-info user-admin');
+    container.appendChild(admin);
+}
