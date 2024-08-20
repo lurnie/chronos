2e42f83acfec7bab676a167124254d9a4418c15c
lurnie <lurnsp22@gmail.com>
2024-08-19 19:34:23 -0500

    Added confirmation messages

--- frontend/public/scripts/error.js
+++ frontend/public/scripts/error.js
@@ -1,17 +1,28 @@
 async function createErrorElement(response) {
-    const error = document.createElement('div');
-    error.setAttribute('class', 'error-header');
-    const span = document.createElement('span');
     let errorText = await response.text();
-    span.textContent = `Error: ${errorText}`;
-    const x = document.createElement('button');
-    x.textContent = 'X';
-    x.setAttribute('class', 'error-x-button')
-    x.addEventListener('click', () => {
-        error.remove();
-    })
-    error.append(span, x);
+    const error = await createMessageElement(`Error: ${errorText}`);
+    error.setAttribute('class', 'confirmation-box message');
     document.body.append(error);
 }
 
-export {createErrorElement};
+async function createConfirmationElement(text) {
+    const message = await createMessageElement(text);
+    message.setAttribute('class', 'confirmation-box message');
+    document.body.append(message);
+}
+
+async function createMessageElement(text) {
+    const message = document.createElement('div');
+    const span = document.createElement('span');
+    span.textContent = text;
+    const x = document.createElement('button');
+    x.textContent = 'X';
+    x.setAttribute('class', 'x-button')
+    x.addEventListener('click', () => {
+        message.remove();
+    })
+    message.append(span, x);
+    return message;
+}
+
+export {createErrorElement, createConfirmationElement};
